06288e397e126130f8f2073c56ebc97255e30382
Sonoko Mizuki <sonoko@mizuki.io>
2021-07-04 02:04:56 +0900

    Add content hash

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 // Copyright (C) 2021 Mizuki Sonoko. All rights reserved.
 
 use clap::{App, Arg};
+extern crate hex;
 extern crate web3;
 
+mod ens;
+mod resolver;
+
+use ens::EnsContract;
+use resolver::ResolverContract;
+
+use anyhow::Result;
+use bs58;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::collections::HashMap;
 use std::{env, process};
+use thiserror::Error;
 use tiny_keccak::{Hasher, Keccak};
-use web3::{
-    contract::{Contract, Options},
-    types::{Address, H256},
-    Transport,
-};
+use web3::types::H256;
 
 pub fn namehash(name: &str) -> Vec<u8> {
     let mut node = vec![0u8; 32];
@@ -34,89 +43,72 @@ pub fn namehash(name: &str) -> Vec<u8> {
     node
 }
 
-fn is_name(val: &str) -> Result<(), String> {
+fn is_name(val: &str) -> Result<()> {
     if val.ends_with(".eth") {
         Ok(())
     } else {
-        Err(String::from("the name format must be ***.eth"))
+        Err(WiseError::InvalidArgvName(
+            "the name format must be ***.eth".to_string(),
+        ))?
     }
 }
 
-// See: https://etherscan.io/address/0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e
-// Doc: https://docs.ens.domains/ens-deployments
-const ENS_CONTRACT_ADDR: &str = "00000000000C2E074eC69A0dFb2997BA6C7d2e1e";
-
-struct EnsContract<T: Transport> {
-    contract: Contract<T>,
+fn strip(val: &str) -> String {
+    return val[1..val.len() - 1].to_string();
 }
 
-impl<T: Transport> EnsContract<T> {
-    pub fn new(web3: web3::Web3<T>) -> Self {
-        EnsContract {
-            contract: Contract::from_json(
-                web3.eth(),
-                ENS_CONTRACT_ADDR.parse().unwrap(),
-                include_bytes!("../contracts/ENS.abi"),
-            )
-            .unwrap(),
-        }
-    }
+#[derive(Error, Debug)]
+pub enum WiseError {
+    #[error("decode failed")]
+    DecodeFailed(hex::FromHexError),
+    #[error("decode failed content hash to utf8")]
+    InvalidContentHash(std::string::FromUtf8Error),
+    #[error("the name format must be ***.eth")]
+    InvalidArgvName(String),
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
+}
 
-    pub async fn owner(&self, namehash: &H256) -> Result<Address, web3::contract::Error> {
-        self.contract
-            .query("owner", (*namehash,), None, Options::default(), None)
-            .await
-    }
-
-    pub async fn resolver(&self, namehash: &H256) -> Result<Address, web3::contract::Error> {
-        self.contract
-            .query("resolver", (*namehash,), None, Options::default(), None)
-            .await
+// Supported type is ipfs, swarm, ipns.
+// Referring source https://github.com/pldespaigne/content-hash/blob/master/src/profiles.js
+fn decode_content_hash(val: &str) -> Result<(String, String)> {
+    if val.starts_with("0xe3") {
+        // ipfs-ns
+        // Note: ens's content hash is
+        //    0xe301017012205cf128dcc4ef93cb5b900d30540ce1ab25328e450c7f5f9b3a6d338a2f8c1294
+        // IPFS hash is 12205cf128dcc4ef93cb5b900d30540ce1ab25328e450c7f5f9b3a6d338a2f8c1294
+        // So remove prefix 0xe3010170
+        let vec = hex::decode(&val[10..val.len()])?;
+        return Ok((bs58::encode(&vec).into_string(), "ipfs-ns".to_string()));
+    } else if val.starts_with("0xe4") {
+        // swarm-ns
+        return Ok((
+            String::from_utf8(hex::decode(&val[2..val.len()])?)?,
+            "swarm-ns".to_string(),
+        ));
+    } else if val.starts_with("0xe5") {
+        // ipns-ns
+        let vec = hex::decode(&val[11..val.len() - 1])?;
+        return Ok((bs58::encode(&vec).into_string(), "ipns-ns".to_string()));
+    } else {
+        return Ok((
+            String::from_utf8(hex::decode(&val[2..val.len()])?)?,
+            "utf-8".to_string(),
+        ));
     }
 }
 
-struct ResolverContract<T: Transport> {
-    contract: Contract<T>,
-}
-
-impl<T: Transport> ResolverContract<T> {
-    pub fn new(addr: web3::types::Address, web3: web3::Web3<T>) -> Self {
-        ResolverContract {
-            contract: Contract::from_json(
-                web3.eth(),
-                addr,
-                include_bytes!("../contracts/PublicResolver.abi"),
-            )
-            .unwrap(),
-        }
-    }
-
-    pub async fn text(
-        &self,
-        namehash: &H256,
-        name: String,
-    ) -> Result<String, web3::contract::Error> {
-        let res = self
-            .contract
-            .query("text", (*namehash, name), None, Options::default(), None);
-        let r = res.await.expect("text.result.wait()");
-        Ok(r)
-    }
-
-    pub async fn print_text(&self, namehash: &H256, name: String) {
-        match self.text(&namehash, name.to_string()).await {
-            Ok(value) => {
-                if !value.is_empty() {
-                    println!(" {:<12}: {}", name, value)
-                } else {
-                    println!(" {:<12}: Not set", name)
-                }
-            }
-            Err(err) => {
-                println!("\terr: {}", err)
-            }
-        }
-    }
+#[derive(Serialize, Deserialize)]
+struct EnsInfo {
+    owner: web3::types::H160,
+    resolver: web3::types::H160,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    content_hash: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    content_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    decoded_content_hash: Option<String>,
+    text_record: HashMap<String, String>,
 }
 
 #[tokio::main]
@@ -132,13 +124,14 @@ async fn main() -> web3::Result<()> {
         .arg(
             Arg::new("json")
                 .short('j')
+                .long("json")
                 .about("Result will be json format"),
         )
         .get_matches();
 
-    if matches.is_present("json") {
-        println!("json is not supported");
-    }
+    // Option
+    let is_json = matches.is_present("json");
+
     let ens_name = matches.value_of("name").unwrap();
     let ens_namehash: H256 = H256::from_slice(namehash(ens_name).as_slice());
     let provider_addr = match env::var("WEB3_PROVIDER") {
@@ -150,25 +143,39 @@ async fn main() -> web3::Result<()> {
     };
     let transport = web3::transports::Http::new(&provider_addr)?;
     let ens = EnsContract::new(web3::Web3::new(&transport));
+    let mut ens_info = EnsInfo {
+        owner: web3::types::H160([0u8; 20]),
+        resolver: web3::types::H160([0u8; 20]),
+        content_hash: None,
+        content_type: None,
+        decoded_content_hash: None,
+        text_record: HashMap::new(),
+    };
 
     match ens.owner(&ens_namehash).await {
         Ok(owner) => {
             if owner == web3::types::H160([0u8; 20]) {
-                println!("owner not found name:{}\n", ens_name);
+                if is_json {
+                    println!(
+                        "{}",
+                        json!({ "result": format!("owner not found name: {}", ens_name) })
+                            .to_string()
+                    );
+                    process::exit(0);
+                }
+                println!("owner not found name: {}\n", ens_name);
                 process::exit(0);
             }
-            println!("\nowner: {:?}", owner);
+            ens_info.owner = owner;
         }
         Err(err) => {
             println!("err: {}", err)
         }
     }
-
     match ens.resolver(&ens_namehash).await {
         Ok(rslv_addr) => {
-            println!("resolver: {:?}", rslv_addr);
+            ens_info.resolver = rslv_addr;
             let resolver = ResolverContract::new(rslv_addr, web3::Web3::new(&transport));
-            println!("\n-------\n");
             let params = vec![
                 "vnd.twitter",
                 "vnd.github",
@@ -179,15 +186,70 @@ async fn main() -> web3::Result<()> {
                 "notice",
                 "keywords",
                 "com.twitter",
+                "snapshot",
                 "com.github",
             ];
             for param in params.iter() {
-                resolver.print_text(&ens_namehash, param.to_string()).await;
+                match resolver.text(&ens_namehash, param.to_string()).await {
+                    Ok(value) => {
+                        if !value.is_empty() {
+                            ens_info.text_record.insert(param.to_string(), value);
+                        }
+                    }
+                    Err(err) => {
+                        println!("\terr: {}", err)
+                    }
+                }
+            }
+
+            match resolver.content_hash(&ens_namehash).await {
+                Ok(bytes_content_hash) => {
+                    let content_hash = strip(
+                        &String::from_utf8(serde_json::to_vec(&bytes_content_hash).unwrap())
+                            .unwrap(),
+                    );
+                    if content_hash != "0x" {
+                        ens_info.content_hash = Some(content_hash);
+                        if ens_info.content_hash.is_some() {
+                            let decoded =
+                                decode_content_hash(ens_info.content_hash.as_ref().unwrap())
+                                    .expect("decode failed");
+                            ens_info.decoded_content_hash = Some(decoded.0);
+                            ens_info.content_type = Some(decoded.1);
+                        }
+                    }
+                }
+                Err(err) => {
+                    println!("\terr: {}", err)
+                }
             }
         }
         Err(err) => {
             println!("\terr: {}", err)
         }
+    }
+
+    if is_json {
+        println!("{}", json!(ens_info).to_string());
+    } else {
+        println!("       owner: {:?}", ens_info.owner);
+        println!("    resolver: {:?}", ens_info.resolver);
+        println!("\n-------\n");
+        if ens_info.content_hash.is_some() {
+            println!(" content_hash : {}", &ens_info.content_hash.unwrap());
+            println!(
+                " decoded_hash : {}",
+                &ens_info.decoded_content_hash.unwrap()
+            );
+            println!(" content_type : {}", &ens_info.content_type.unwrap());
+        } else {
+            println!("content_hash: Not set");
+        }
+        println!("\n-------\n");
+        for (name, value) in &ens_info.text_record {
+            println!(" {:<12}: {}", name, value);
+        }
+        println!("")
     }
 
     Ok(())
